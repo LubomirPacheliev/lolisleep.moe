5e3c7d4790bb8489f0a7ed5bd49144e63f04ec05
Sagar <67011477+thevenuz@users.noreply.github.com>
2023-10-01 13:19:57 -0500

    update github repo link

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -31,7 +31,7 @@ const Footer: React.FunctionComponent<Props> = (props: Props) => {
                 onClick={() => window.location.href = "/"} onMouseEnter={() => setLogoHover(true)} onMouseLeave={() => setLogoHover(false)}/>
                 <p className="footer-text">Listen to soft loli breathing and moaning. <br/>Have a good time sleeping!</p>
                 <p className="footer-text active" onClick={download}>Download</p>
-                <p className="footer-text active" onClick={() => window.open("https://github.com/Tenpi/lolisleep.moe", "__blank")}>Github</p>
+                <p className="footer-text active" onClick={() => window.open("https://github.com/LubomirPacheliev/lolisleep.moe", "__blank")}>Github</p>
                 <p className="footer-text-small">Copyright © {new Date().getFullYear()} Tenpi</p>
             </div>
             <img src={laffey} className="laffey" width="240" height="288"/>
